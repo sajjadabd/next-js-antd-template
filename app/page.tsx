9f37074094a3e7e8d941b6307d17394823ce5e3b
sajjadabd <mynameissajjad@gmail.com>
2023-07-02 21:31:57 +0330

    make persian menu

--- app/page.tsx
+++ app/page.tsx
@@ -35,30 +35,54 @@ function getItem(
   } as MenuItem;
 }
 
+const section = 'سکشن'
+
+const barmill = 'بار میل'
+
 const items: MenuProps['items'] = [
   // getItem('Navigation One', 'sub1', <MailOutlined />, [
   //   getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
   //   getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
   // ]),
 
-  getItem('Navigation One', 'sub1', <AppstoreOutlined />, [
-    getItem( <Link href="/login">
-        Login
-    </Link>, '5'),
-    getItem(<Link href="/management">
-        Management
-    </Link>, '6'),
+  getItem('ایجاد غلطک', 'sub1', <AppstoreOutlined />, [
+    getItem( <Link href="/createroller/section">
+        {section}
+    </Link>, '1'),
+    getItem(<Link href="/createroller/barmill">
+        {barmill}
+    </Link>, '2'),
     // getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
   ]),
 
-  { type: 'divider' },
+  // { type: 'divider' },
 
-  getItem('Navigation Two', 'sub2', <SettingOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
-    getItem('Option 11', '11'),
-    getItem('Option 12', '12'),
+  getItem('تعریف خط', 'sub2', <SettingOutlined />, [
+    getItem(<Link href="/defineline/section">{section}</Link>, '3'),
+    getItem(<Link href="/defineline/barmill">{barmill}</Link>, '4'),
   ]),
+
+  getItem('مدیریت خط', 'sub3', <SettingOutlined />, [
+    getItem(<Link href="/defineline/section">{section}</Link>, '5'),
+    getItem(<Link href="/defineline/barmill">{barmill}</Link>, '6'),
+  ]),
+
+  getItem('ثبت تراش غلطک ها', 'sub4', <SettingOutlined />, [
+    getItem(<Link href="/defineline/section">{section}</Link>, '7'),
+    getItem(<Link href="/defineline/barmill">{barmill}</Link>, '8'),
+  ]),
+
+  getItem('تغییر وضعیت غلطک ها', 'sub5', <SettingOutlined />, [
+    getItem(<Link href="/defineline/section">{section}</Link>, '9'),
+    getItem(<Link href="/defineline/barmill">{barmill}</Link>, '10'),
+  ]),
+
+  getItem('مدیریت استند ها', 'sub6', <SettingOutlined />, [
+    getItem(<Link href="/defineline/section">{section}</Link>, '11'),
+    getItem(<Link href="/defineline/barmill">{barmill}</Link>, '12'),
+  ]),
+
+
 
   //getItem('Group', 'grp', null, [getItem('Option 13', '13'), getItem('Option 14', '14')], 'group'),
 ];
@@ -116,25 +140,26 @@ export default function App () {
           defaultSelectedKeys={['1']}
           defaultOpenKeys={['sub1']}
           mode="inline"
-          theme={theme}
+          // theme={theme}
           inlineCollapsed={collapsed}
-          // theme="dark"
+          theme="light"
           items={items}
+          className={styles.menu}
         />
       </nav>
 
       <div className={styles.content}>
         <div className={styles.gadgets}>
-        <Button className={styles.navigationResponsiveButton} type={collapsed ? "primary" : "secondary"} onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+        <Button className={styles.navigationResponsiveButton} type={collapsed ? "primary" : "text"} onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
           {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
         </Button>
-        <Switch
+        {/* <Switch
           checked={theme === 'dark'}
           onChange={changeTheme}
           checkedChildren="Dark"
           unCheckedChildren="Light"
           className={styles.switch}
-        />
+        /> */}
         </div>
         <div className={styles.mainContent}>
           main content
