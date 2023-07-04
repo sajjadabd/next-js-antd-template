3ca66a8232b882a28daf1610b3738b721b194d54
sajjadabd <mynameissajjad@gmail.com>
2023-07-04 02:54:20 -0700

    make more pages

--- app/(actions)/manageline/barmill/page.tsx
+++ app/(actions)/manageline/barmill/page.tsx
@@ -2,10 +2,99 @@
 
 import React , { useState } from 'react';
 
+import { Timeline } from 'antd';
+
 export default function App () {
   return (
     <div>
-        manage line / barmill
+        <Timeline
+            items={[
+            {
+                color: 'gray',
+                children: (
+                <>
+                    <h4>استند 1</h4>
+                    <ul>
+                        <li>غلطک بالا</li>
+                        <li>غلطک پایین</li>
+                    </ul>
+                </>
+                ),
+            },
+            {
+                color: 'gray',
+                children: (
+                <>
+                    <h4>استند 2</h4>
+                    <ul>
+                        <li>غلطک بالا</li>
+                        <li>غلطک پایین</li>
+                    </ul>
+                </>
+                ),
+            },
+            {
+                color: 'gray',
+                children: (
+                <>
+                    <h4>استند 3</h4>
+                    <ul>
+                        <li>غلطک بالا</li>
+                        <li>غلطک پایین</li>
+                    </ul>
+                </>
+                ),
+            },
+            {
+                color : 'gray',
+                children: (
+                <>
+                    <h4>استند 4</h4>
+                    <ul>
+                        <li>غلطک بالا</li>
+                        <li>غلطک پایین</li>
+                    </ul>
+                </>
+                ),
+            },
+            {
+                color: 'gray',
+                children: (
+                <>
+                    <h4>استند 5</h4>
+                    <ul>
+                        <li>غلطک بالا</li>
+                        <li>غلطک پایین</li>
+                    </ul>
+                </>
+                ),
+            },
+            {
+                color: 'gray',
+                children: (
+                <>
+                    <h4>استند 6</h4>
+                    <ul>
+                        <li>غلطک بالا</li>
+                        <li>غلطک پایین</li>
+                    </ul>
+                </>
+                ),
+            },
+            {
+                color: 'gray',
+                children: (
+                <>
+                    <h4>استند 7</h4>
+                    <ul>
+                        <li>غلطک بالا</li>
+                        <li>غلطک پایین</li>
+                    </ul>
+                </>
+                ),
+            },
+            ]}
+        />
     </div>
   );
 };
