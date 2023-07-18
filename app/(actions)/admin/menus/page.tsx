1d5b7acc0f581a226bf6f50f296ecbd1c371b913
sajjadabd <mynameissajjad@gmail.com>
2023-07-18 02:17:11 -0700

    make menu management more modular

--- app/(actions)/admin/menus/page.tsx
+++ app/(actions)/admin/menus/page.tsx
@@ -8,50 +8,7 @@ import { Button, Table } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 
 
-interface DataType {
-  key: React.Key;
-  name: string;
-  age: number;
-  address: string;
-}
-
-
-const dataSource = [
-  {
-    key: '1',
-    name: 'John Doe',
-    age: 25,
-    address: '123 ABC Street',
-  },
-  {
-    key: '2',
-    name: 'Jane Smith',
-    age: 32,
-    address: '456 XYZ Street',
-  },
-];
-
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
-
-
-
+import Body from './body';
 
 
 
@@ -63,85 +20,10 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Example />
+      <Body />
     </QueryClientProvider>
   )
 }
 
 
-
-export function Example () {
-
-
-  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  const start = () => {
-    setLoading(true);
-    // ajax request after empty completing
-    setTimeout(() => {
-      setSelectedRowKeys([]);
-      setLoading(false);
-    }, 1000);
-  };
-
-
-  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
-    setSelectedRowKeys(newSelectedRowKeys);
-  };
-
-  const rowSelection = {
-    selectedRowKeys,
-    onChange: onSelectChange,
-  };
-  const hasSelected = selectedRowKeys.length > 0;
-
-
-  const { isLoading, error, data } = useQuery('repoData', () => 
-    fetch('http://127.0.0.1:8000').then(res =>
-      res.json()
-    )
-  )
-
-
-
-
-
-
-  if (isLoading) return 'Loading...'
-
-  if (error) return 'An error has occurred: ' + error
-
-  console.log(data)
-
-
-
-
-
-
-  return (
-    <div>
-        admin / menus
-        <div>
-          {data.message}
-        </div>
-
-        <div>
-          <div style={{ marginBottom: 16 }}>
-            {/* <Button type="primary" onClick={start} disabled={!hasSelected} loading={loading}>
-              Reload
-            </Button> */}
-            <span style={{ marginLeft: 8 }}>
-              {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
-            </span>
-          </div>  
-          <Table rowSelection={rowSelection} dataSource={dataSource} columns={columns} />
-        </div>
-        
-
-
-    </div>
-  );
-};
 
