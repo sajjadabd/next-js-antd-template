60ab19893b9d97f5a7cad0ce5eafa812c7f1a9f4
sajjadabd <mynameissajjad@gmail.com>
2023-07-19 00:52:36 -0700

    change api request to real server

--- api/request.ts
+++ api/request.ts
@@ -3,7 +3,7 @@ let baseUrl : string = "http://127.0.0.1:8000"
 let baseServer : string = "https://80.82.68.126"
 
 
-let url = baseUrl;
+let url = baseServer;
 
 let MenuCreationPath = url + "/menu/create"
 
